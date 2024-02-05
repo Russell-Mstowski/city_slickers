9806576aebb8d6cca93a4f9f9c79925b4f16c95e
Russell Mstowski <russell.mstowski@gmail.com>
2024-02-04 22:35:12 -0600

    add relationships / add avg rating method / finish pt 1

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,2 +1,8 @@
 class Place < ApplicationRecord
+  has_many :ratings
+
+  # Average rating for a given Place rounded to 2 decimal places
+  def rating
+    self.ratings.average("value").round(2)
+  end
 end
--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,2 +1,9 @@
 class Rating < ApplicationRecord
+  belongs_to :place
+
+  # Validate that a Place is associated with a Rating
+  validates_associated :place
+
+  # Validate a value is present
+  validates :value, presence: true
 end
