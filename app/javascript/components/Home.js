095e87578efaaa3aba20b147e94e17488e627463
Russell Mstowski <russell.mstowski@gmail.com>
2024-02-06 14:10:05 -0600

    add comments / cleanup

--- app/javascript/components/Home.js
+++ app/javascript/components/Home.js
@@ -79,6 +79,7 @@ const Home = () => {
 
   return (
     <React.Fragment>
+      {/* TODO: Break out into component */}
       <div className="header">
         <h1>City Slicker</h1>
         
@@ -94,47 +95,48 @@ const Home = () => {
         <div style={{ textAlign: "center", marginTop: 100 }}>loading...</div>
       ) : (
         <div>
-            <div>
-              <div className="table-content fade-in">
-                <table>
-                  <thead>
-                    <tr>
-                      <th scope="col">Name</th>
-                      <th scope="col" align="left">Description</th>
-                      <th scope="col">Rating</th>
-                    </tr>
-                  </thead>
-                  { places.length > 0 ? (
-                    <tbody>
-                      { places.map(place => 
-                        <tr key={place.id}>
-                          <td width={150}>{place?.name.toLowerCase()}</td>
-                          <td>{formatDescription(place?.description)}</td>
-                          <td><b>{place?.average_rating}</b></td>
-                        </tr>
-                      )}
-                    </tbody>
-                  ) : (
-                    <tr style={{ textAlign: "center", height: 40}}>
-                      <td colspan="100%">
-                        No places.
-                      </td>
-                    </tr>
-                  )}
-                </table>
-              </div>
+            {/* TODO: Break out into component */}
+            <div className="table-content fade-in">
+              <table>
+                <thead>
+                  <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col" align="left">Description</th>
+                    <th scope="col">Rating</th>
+                  </tr>
+                </thead>
+                { places.length > 0 ? (
+                  <tbody>
+                    { places.map(place => 
+                      <tr key={place.id}>
+                        <td width={150}>{place?.name.toLowerCase()}</td>
+                        <td>{formatDescription(place?.description)}</td>
+                        <td><b>{place?.average_rating}</b></td>
+                      </tr>
+                    )}
+                  </tbody>
+                ) : (
+                  <tr style={{ textAlign: "center", height: 40}}>
+                    <td colspan="100%">
+                      No places.
+                    </td>
+                  </tr>
+                )}
+              </table>
             </div>
           
-
-            <div className="page-btns">
+            <div className="pagination-btns">
               { page > 1 ? (
                 <button className="previous" onClick={() => {previousPage()}}>Previous</button>
               ) : ("")}
 
-              {page}
 
               { places.length !== 0 ? (
-                <button className="next" onClick={() => {nextPage()}}>Next</button>
+                <div>
+                  {page}
+
+                  <button className="next" onClick={() => {nextPage()}}>Next</button>
+                </div>
               ): ("")}
             </div>
         </div>
