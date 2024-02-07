70a0b91ff6f97590af9e095003571b753776ed20
Russell Mstowski <russell.mstowski@gmail.com>
2024-02-06 21:32:29 -0600

    add model specs

--- spec/models/place_spec.rb
+++ spec/models/place_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Place, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:place) { FactoryBot.create(:place) }
+  let!(:rating) { FactoryBot.build(:rating, place_id: place.id) }
+  
+  before { rating.save! }
+
+  describe "place model methods" do
+    it "returns the average rating of a place" do
+      expect(place.rating).to eq(rating.value)
+    end
+  end
+
+  describe "place model validations" do
+    it "validates name and description are upcased" do
+      expect(place.name).to eq("Chicago".upcase)
+      expect(place.description).to eq("As a multicultural city that thrives on the harmony and diversity of its neighborhoods, Chicago today embodies the values of America's heartland-integrity, hard work and community and reflects the ideals in the social fabric of its 77 distinct neighborhoods.".upcase)
+    end
+
+    it "validates lat and long are numeric" do
+      expect(place.latitude).to be_a_kind_of(Numeric)
+      expect(place.longitude).to be_a_kind_of(Numeric)
+    end
+  end
 end
--- spec/models/rating_spec.rb
+++ spec/models/rating_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Rating, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:place) { FactoryBot.create(:place) }
+  let!(:rating) { FactoryBot.build(:rating, place_id: place.id) }
+  
+  before { rating.save! }
+
+  describe "rating model validations" do
+    it "validates a place exists and value is numeric" do
+      expect(rating.place_id).to eq(place.id)
+      expect(rating.value).to be_a_kind_of(Numeric)
+    end
+  end
 end
