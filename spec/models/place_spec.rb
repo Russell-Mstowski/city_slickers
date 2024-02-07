851f4f640033de3d03a0f40df8cfee38317e5bfb
Russell Mstowski <russell.mstowski@gmail.com>
2024-02-06 21:38:53 -0600

    check successful model save

--- spec/models/place_spec.rb
+++ spec/models/place_spec.rb
@@ -6,13 +6,11 @@ RSpec.describe Place, type: :model do
   
   before { rating.save! }
 
-  describe "place model methods" do
-    it "returns the average rating of a place" do
-      expect(place.rating).to eq(rating.value)
-    end
-  end
-
   describe "place model validations" do
+    it "should save successfully" do
+      expect(place.save).to eq(true)
+    end
+
     it "validates name and description are upcased" do
       expect(place.name).to eq("Chicago".upcase)
       expect(place.description).to eq("As a multicultural city that thrives on the harmony and diversity of its neighborhoods, Chicago today embodies the values of America's heartland-integrity, hard work and community and reflects the ideals in the social fabric of its 77 distinct neighborhoods.".upcase)
@@ -21,6 +19,12 @@ RSpec.describe Place, type: :model do
     it "validates lat and long are numeric" do
       expect(place.latitude).to be_a_kind_of(Numeric)
       expect(place.longitude).to be_a_kind_of(Numeric)
+    end
+  end
+
+  describe "place model methods" do
+    it "returns the average rating of a place" do
+      expect(place.rating).to eq(rating.value)
     end
   end
 end
--- spec/models/rating_spec.rb
+++ spec/models/rating_spec.rb
@@ -4,10 +4,15 @@ RSpec.describe Rating, type: :model do
   let!(:place) { FactoryBot.create(:place) }
   let!(:rating) { FactoryBot.build(:rating, place_id: place.id) }
   
-  before { rating.save! }
-
   describe "rating model validations" do
+    it "should save successfully" do
+      expect(rating.save).to eq(true)
+    end
+
+    before { rating.save! }
+    
     it "validates a place exists and value is numeric" do
+
       expect(rating.place_id).to eq(place.id)
       expect(rating.value).to be_a_kind_of(Numeric)
     end
