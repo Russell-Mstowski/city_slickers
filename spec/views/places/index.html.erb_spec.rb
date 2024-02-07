dbd5f05e7fbad4e38ad2ecd3c44911b7f4c2c965
Russell Mstowski <russell.mstowski@gmail.com>
2024-02-06 23:22:53 -0600

    pass places/index

--- spec/views/places/index.html.erb_spec.rb
+++ spec/views/places/index.html.erb_spec.rb
@@ -1,29 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe "places/index", type: :view do
+  let!(:places) { [ FactoryBot.create(:place) ] }
+
   before(:each) do
-    assign(:places, [
-      Place.create!(
-        latitude: "9.99",
-        longitude: "9.99",
-        name: "Name",
-        description: "MyText"
-      ),
-      Place.create!(
-        latitude: "9.99",
-        longitude: "9.99",
-        name: "Name",
-        description: "MyText"
-      )
-    ])
+    assign(:places, places)
   end
 
   it "renders a list of places" do
     render
     cell_selector = Rails::VERSION::STRING >= '7' ? 'div>p' : 'tr>td'
-    assert_select cell_selector, text: Regexp.new("9.99".to_s), count: 2
-    assert_select cell_selector, text: Regexp.new("9.99".to_s), count: 2
-    assert_select cell_selector, text: Regexp.new("Name".to_s), count: 2
-    assert_select cell_selector, text: Regexp.new("MyText".to_s), count: 2
+    assert_select cell_selector, text: Regexp.new(places.first.name.to_s), count: 2
+    assert_select cell_selector, text: Regexp.new(places.first.description.to_s), count: 1
+    assert_select cell_selector, text: Regexp.new(places.first.latitude.to_s), count: 1
+    assert_select cell_selector, text: Regexp.new(places.first.longitude.to_s), count: 1
   end
 end
