7105cde5adba191402715904b7c0dced9ad950cf
Russell Mstowski <russell.mstowski@gmail.com>
2024-02-06 23:17:05 -0600

    pass places/show

--- spec/views/places/show.html.erb_spec.rb
+++ spec/views/places/show.html.erb_spec.rb
@@ -1,20 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe "places/show", type: :view do
+  let!(:place) { FactoryBot.create(:place) }
+
   before(:each) do
-    assign(:place, Place.create!(
-      latitude: "9.99",
-      longitude: "9.99",
-      name: "Name",
-      description: "MyText"
-    ))
+    assign(:place, place)
   end
 
   it "renders attributes in <p>" do
     render
-    expect(rendered).to match(/9.99/)
-    expect(rendered).to match(/9.99/)
-    expect(rendered).to match(/Name/)
-    expect(rendered).to match(/MyText/)
+    expect(rendered).to match(/#{place.name}/)
+    expect(rendered).to match(/AS A MULTICULTURAL CITY THAT THRIVES ON THE HARMONY AND DIVERSITY OF ITS NEIGHBORHOODS, CHICAGO TODAY EMBODIES THE VALUES OF AMERICA&#39;S HEARTLAND-INTEGRITY, HARD WORK AND COMMUNITY AND REFLECTS THE IDEALS IN THE SOCIAL FABRIC OF ITS 77 DISTINCT NEIGHBORHOODS./)
+    expect(rendered).to match(/#{place.longitude}/)
+    expect(rendered).to match(/#{place.longitude}/)
   end
 end
