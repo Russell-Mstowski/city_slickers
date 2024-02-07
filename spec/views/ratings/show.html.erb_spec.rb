ec458f8376a45c05e469f2ca33b3ff74f8185046
Russell Mstowski <russell.mstowski@gmail.com>
2024-02-06 22:34:45 -0600

    pass ratings/show

--- spec/views/ratings/show.html.erb_spec.rb
+++ spec/views/ratings/show.html.erb_spec.rb
@@ -2,17 +2,15 @@ require 'rails_helper'
 
 RSpec.describe "ratings/show", type: :view do
   let!(:place) { FactoryBot.create(:place) }
+  let!(:rating) { FactoryBot.create(:rating, place_id: place.id) }
 
   before(:each) do
-    assign(:rating, Rating.create!(
-      place_id: place.id,
-      value: 3.5
-    ))
+    assign(:rating, rating)
   end
 
   it "renders attributes in <p>" do
     render
-    expect(rendered).to match(/2/)
-    expect(rendered).to match(/3.5/)
+    expect(rendered).to match(/#{place.id}/)
+    expect(rendered).to match(/#{rating.value}/)
   end
 end
