5502263ab2babf24765b411696ea2ef3e23c8fee
Russell Mstowski <russell.mstowski@gmail.com>
2024-02-06 22:40:33 -0600

    pass ratings/index

--- spec/views/ratings/index.html.erb_spec.rb
+++ spec/views/ratings/index.html.erb_spec.rb
@@ -1,23 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe "ratings/index", type: :view do
+  let!(:place) { FactoryBot.create(:place) }
+  let!(:ratings) { [ FactoryBot.build(:rating, place_id: place.id), FactoryBot.build(:rating, place_id: place.id) ] }
+  
   before(:each) do
-    assign(:ratings, [
-      Rating.create!(
-        place_id: 2,
-        value: 3.5
-      ),
-      Rating.create!(
-        place_id: 2,
-        value: 3.5
-      )
-    ])
+    assign(:ratings, ratings)
   end
 
   it "renders a list of ratings" do
     render
     cell_selector = Rails::VERSION::STRING >= '7' ? 'div>p' : 'tr>td'
-    assert_select cell_selector, text: Regexp.new(2.to_s), count: 2
-    assert_select cell_selector, text: Regexp.new(3.5.to_s), count: 2
+    assert_select cell_selector, text: Regexp.new(place.id.to_s), count: 2
+    assert_select cell_selector, text: Regexp.new(ratings.first.value.to_s), count: 1
+    assert_select cell_selector, text: Regexp.new(ratings.second.value.to_s), count: 1
   end
 end
